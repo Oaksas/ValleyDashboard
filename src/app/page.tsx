7d551bdac79a58f7e702177c61d7fe9e140ff1e0
=oaksas <=>
2024-02-28 07:54:57 +0300

    Refactore

---
+++ src/app/page.tsx
@@ -0,0 +1,63 @@
+import Head from 'next/head';
+import * as React from 'react';
+import '@/lib/env';
+
+import ComponentPage from '@/app/components';
+import Hero from '@/app/components/Hero/Hero';
+import Hotdeal from '@/app/components/Hotdeal/Hotdeal';
+import Navbar from '@/app/components/Navbar/Navbar';
+import OtherDeals from '@/app/components/OtherDeals/OtherDeals';
+import { BannerCarouselProps, Deal, MainShortcut } from '@/app/types';
+import {
+  getBannerData,
+  getHotDealsData,
+  getShortcutData,
+} from '@/app/util/store';
+
+const HomePage = async () => {
+  let Bannerdata: BannerCarouselProps | null;
+  let ShortcutData: MainShortcut[] | null;
+  let HotdealData: Deal | null;
+
+  try {
+    Bannerdata = await getBannerData();
+    ShortcutData = await getShortcutData();
+    HotdealData = await getHotDealsData();
+  } catch (error) {
+    Bannerdata = null;
+    ShortcutData = null;
+    HotdealData = null;
+  }
+
+  return (
+    <main>
+      <Head>
+        <title>TesValley</title>
+      </Head>
+      <div className="mx-10 my-6">
+        <Navbar />
+        <hr />
+        {Bannerdata ? (
+          <ComponentPage data={Bannerdata} />
+        ) : (
+          <div className="error-message">
+            Error fetching data. Please try again later.
+          </div>
+        )}
+      </div>
+      <div className="mx-auto mt-3 flex w-11/12 flex-col justify-center lg:w-8/12">
+        {ShortcutData && <Hero data={ShortcutData} />}
+        {HotdealData && <Hotdeal data={HotdealData.items[0]} />}
+        {HotdealData?.items &&
+          HotdealData.items.length > 0 &&
+          HotdealData.items
+            .slice(1)
+            .map((item: any, index: number) => (
+              <OtherDeals key={index} data={item} />
+            ))}
+      </div>
+    </main>
+  );
+};
+
+export default HomePage;
---
+++ src/constant/config.ts
@@ -0,0 +1,4 @@
+export const siteConfig = {
+  title: 'TestValley',
+  description: 'A TestValley assignment pre-configured with Husky',
+};
