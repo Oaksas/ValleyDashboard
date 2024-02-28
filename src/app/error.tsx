4b77d6a85c6c3491938de773f0b0cd3bf8478d99
oaksas <aman.getnet2@gmail.com>
2024-02-28 21:39:07 +0300

    Fix error boundary

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -14,21 +14,20 @@ export default function Error({
 }) {
   React.useEffect(() => {
     // eslint-disable-next-line no-console
-    console.error(error);
   }, [error]);
 
   return (
     <main>
-      <section className='bg-white'>
-        <div className='layout flex min-h-screen flex-col items-center justify-center text-center text-black'>
+      <section className="bg-white">
+        <div className="layout flex min-h-screen flex-col items-center justify-center text-center text-black">
           <RiAlarmWarningFill
             size={60}
-            className='drop-shadow-glow animate-flicker text-red-500'
+            className="drop-shadow-glow animate-flicker text-red-500"
           />
-          <h1 className='mt-8 text-4xl md:text-6xl'>
+          <h1 className="mt-8 text-4xl md:text-6xl">
             Oops, something went wrong!
           </h1>
-          <TextButton variant='basic' onClick={reset} className='mt-4'>
+          <TextButton variant="basic" onClick={reset} className="mt-4">
             Try again
           </TextButton>
         </div>
